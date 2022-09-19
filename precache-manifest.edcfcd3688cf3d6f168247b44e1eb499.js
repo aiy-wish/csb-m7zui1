c7018b1ff9e49d02d421f42f451b604f127abcf9
Code Sandbox <hello@codesandbox.io>
2022-09-19 20:06:59 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.edcfcd3688cf3d6f168247b44e1eb499.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8de85316cea97688c08c787efa310e6",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/index.html"
+  },
+  {
+    "revision": "fbcd0c9efe6cf3eadd0b",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "35b711297ed5d2416636",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "fc15e5594c6cd9d773e9",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/css/main.f7a380cb.chunk.css"
+  },
+  {
+    "revision": "e1f96d8db3c02144f43c",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/2.668e4693.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/2.668e4693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbcd0c9efe6cf3eadd0b",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/3.c0c51208.chunk.js"
+  },
+  {
+    "revision": "35b711297ed5d2416636",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/4.251b27a3.chunk.js"
+  },
+  {
+    "revision": "fc15e5594c6cd9d773e9",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/main.026bc9f6.chunk.js"
+  },
+  {
+    "revision": "c27aef99dd2d17b020f9",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/js/runtime-main.5d6a952c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "7c00293b996134efc5b9f69a4060316d",
+    "url": "https://aiy-wish.github.io/csb-m7zui1/static/media/tcg_new.7c00293b.jpg"
+  }
+]);
